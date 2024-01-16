a2e8d9876f5f0b1f18ea820cd217ca7219af08df
Alibaba <abdulmanafaliu414@gmail.com>
2024-01-16 13:40:23 +0000

    changes made

--- src/Components/RecipeDetails.js
+++ src/Components/RecipeDetails.js
@@ -1,16 +1,50 @@
 // src/components/RecipeDetails.js
 import React from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const RecipeDetails = ({ recipe }) => {
+const RecipeDetails = ({ recipe, setSelectedRecipe, setShow }) => {
+  if (!recipe) {
+    return null;
+  }
+  const handleClose = () => {
+    setSelectedRecipe(null);
+  };
+
   return (
-    <div>
-      <h2>{recipe.label}</h2>
-      <p>
-        Ingredients:{" "}
-        {recipe.ingredients.map((ingredient) => ingredient.text).join(", ")}
-      </p>
-      <p>Instructions: {recipe.url}</p>
-    </div>
+    <Dialog open={true} onClose={handleClose}>
+      <DialogTitle>
+        <h2>{recipe.label}</h2>
+      </DialogTitle>
+      <DialogContent>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Ingredients</th>
+              <th>Weight</th>
+            </tr>
+          </thead>
+          <tbody>
+            {recipe.ingredients &&
+              recipe.ingredients.map((ingredient) => (
+                <tr key={ingredient.text}>
+                  <td>{ingredient.text}</td>
+                  <td>{ingredient.weight.toFixed(2)}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        <DialogContentText>{recipe.url}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
